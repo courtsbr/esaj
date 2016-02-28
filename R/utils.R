46c279880e8c5ba21449793eefb9375c99ba594d
jtrecenti <julio.trecenti@gmail.com>
2016-02-28 18:03:47 +0000

    pipe

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,15 @@
 desacentuar <- function(x) {
   gsub("`|\\'", "", iconv(x, to = "ASCII//TRANSLIT"))
 }
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
