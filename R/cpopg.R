d8d3105e7c79c8c7b7cbf4f0b33bc96931b5b1f5
fcorrea <ezeloc2@gmail.com>
2016-08-29 16:10:25 -0300

    baixa com mlply, agora

--- R/cpopg.R
+++ R/cpopg.R
@@ -18,7 +18,7 @@ cpo_pg <- function(processos, path = "data-raw/cpo-pg", tj = 'TJSP', .parallel =
     d <- d %>%
       dplyr::select(-id) %>%
       dplyr::rename(p = n_processo) %>%
-      plyr::mdply(cpo_pg_um)
+      plyr::mlply(cpo_pg_um)
   }
   d <- dplyr::ungroup(d)
   d
