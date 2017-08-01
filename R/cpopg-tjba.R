2079c1734c478ca1fb53326a84247fd39754006e
Caio Lente <ctlente@gmail.com>
2017-08-01 19:19:13 -0300

    TJBA now streamlined

--- R/cpopg-tjba.R
+++ R/cpopg-tjba.R
@@ -3,38 +3,33 @@
 # id <- "0552486-62.2015.8.05.0001"
 baixar_tjba <- function(id, path = '.') {
 
-  # Tentar 10 vezes no máximo
+  # Try at most 10 times
   for (i in 1:10) {
 
-    # Query
-    query <- query_processo(id)
-    id_clean <- gsub('[^0-9]', '', id)
+    # Aux function for breaking captcha
+    break_captcha <- purrr::possibly(captchasaj::decodificar, 'xxxxx')
 
-    # Acesso inicial
-    u_open <- 'http://esaj.tjba.jus.br/cpopg/open.do'
-    r_open <- httr::GET(u_open, httr::set_cookies(NULL))
-
-    # Captcha
-    u_captcha <- 'http://esaj.tjba.jus.br/cpopg/imagemCaptcha.do'
-    r_captcha <- httr::GET(u_captcha)
-    tmp <- tempfile()
-    writeBin(httr::content(r_captcha, "raw"), tmp)
-
-    # Decodificar captcha
-    decodificar <- purrr::possibly(captchasaj::decodificar, 'xxxxx')
-    valor <- decodificar(tmp, captchasaj::modelo$modelo)
-
-    # Preencher query
-    query$vlCaptcha <- valor
-
-    # Baixar processo
+    # Relevant URLs
+    u_captcha <- "http://esaj.tjba.jus.br/cpopg/imagemCaptcha.do"
     u_search <- "http://esaj.tjba.jus.br/cpopg/search.do"
-    file <- sprintf('%s/%s.html', path, id_clean)
-    r <- httr::GET(u_search, query = query,
-                   httr::write_disk(file, overwrite = TRUE))
 
-    # Break
-    if (!tem_captcha(r)) { break }
-    else { file.remove(file) }
+    # Download captcha
+    f_captcha <- tempfile()
+    writeBin(httr::content(httr::GET(u_captcha), "raw"), f_captcha)
+
+    # Create GET query
+    query <- query_processo(id)
+    query$vlCaptcha <- break_captcha(f_captcha, captchasaj::modelo$modelo)
+
+    # Download lawsuit
+    f_lwst <- sprintf("%s/%s.html", path, gsub("[^0-9]", "", id))
+    f_search <- httr::GET(u_search, query = query, httr::write_disk(f_lwst, TRUE))
+
+    # Free temporary file
+    file.remove(f_captcha)
+
+    # Breaking condition
+    if (!tem_captcha(f_search)) { break }
+    else { file.remove(f_lwst) }
   }
 }
