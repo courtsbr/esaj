343d3db4e06c87f9b6c9e7b104eabea8c8e41613
Caio Lente <ctlente@gmail.com>
2017-08-04 12:09:32 -0300

    download_pdf() now returns consistent objects and better status messages

--- R/aux_dje.R
+++ R/aux_dje.R
@@ -30,54 +30,55 @@ get_dje_data <- function(tj) {
       booklets = c(1:4)))
 }
 
-get_dje_link <- function(tj, date, ...) {
+# Call appropriate function to get link depending on TJ
+get_dje_link <- function(tj, date, u_dje, booklet) {
   switch(tj,
     "tjac" = tjac_link(date),
     "tjba" = tjba_link(date),
-    "tjrn" = tjrn_link(date, ..2),
-    "tjsc" = tjsc_link(date, ..2),
-    default_link(date, ...)
+    "tjrn" = tjrn_link(date, booklet),
+    "tjsc" = tjsc_link(date, booklet),
+    default_link(date, u_dje, booklet)
   )
 }
 
-default_link <- function(date, ...) {
-  u_dje <- ..1
+# Get link for many TJ"s DJEs
+default_link <- function(date, u_dje, booklet) {
   date_link <- format(lubridate::as_date(date), "%d/%m/%Y")
-  booklet <- ..2
   stringr::str_c(u_dje, "dtDiario=", date_link, "&cdCaderno=", booklet)
 }
 
+# Get link for TJBA"s DJE
 tjba_link <- function(date) {
   # essa foi dificil!
-  u0 <- 'http://www2.tjba.jus.br/diario/internet/pesquisar.wsp'
+  u0 <- "http://www2.tjba.jus.br/diario/internet/pesquisar.wsp"
   r0 <- httr::GET(u0, httr::config(followlocation = 0L))
-  #u <- httr::headers(r0)[['location']]
+  #u <- httr::headers(r0)[["location"]]
   #r00 <- httr::GET(u)
   #F: comentei essa requisição porque não tem "location" no cabeçalho desse GET.
   token <- r0 %>%
-    httr::content('text', encoding = 'ISO-8859-1') %>%
+    httr::content("text", encoding = "ISO-8859-1") %>%
     xml2::read_html() %>%
-    rvest::html_node(xpath = '//input[@id="wi.token"]') %>%
-    rvest::html_attr('value')
-  b <- list('tmp_origem' = '',
-            'tmp.diario.dt_inicio' = '25/01/2001',
-            'tmp.diario.dt_fim' = '04/02/2100',
-            'tmp.diario.cd_caderno' = '',
-            'tmp.diario.cd_secao' = '',
-            'tmp.diario.pal_chave' = '',
-            'wi.token' = token,
-            'tmp.diario.id_advogado' = '')
-  r1 <- httr::POST(u0, body = b, encode = 'form')
+    rvest::html_node(xpath = "//input[@id="wi.token"]") %>%
+    rvest::html_attr("value")
+  b <- list("tmp_origem" = "",
+            "tmp.diario.dt_inicio" = "25/01/2001",
+            "tmp.diario.dt_fim" = "04/02/2100",
+            "tmp.diario.cd_caderno" = "",
+            "tmp.diario.cd_secao" = "",
+            "tmp.diario.pal_chave" = "",
+            "wi.token" = token,
+            "tmp.diario.id_advogado" = "")
+  r1 <- httr::POST(u0, body = b, encode = "form")
   d_edicoes <- r1 %>%
-    httr::content('text', encoding = 'ISO-8859-1') %>%
+    httr::content("text", encoding = "ISO-8859-1") %>%
     xml2::read_html() %>%
-    rvest::html_node('table.grid') %>%
+    rvest::html_node("table.grid") %>%
     rvest::html_table() %>%
     janitor::clean_names() %>%
     #os nomes podem vir repetidos
     tidyr::gather() %>%
-    dplyr::filter(stringr::str_trim(value) != '') %>%
-    tidyr::separate(value, c('edicao', 'date'), sep = '\\(') %>%
+    dplyr::filter(stringr::str_trim(value) != "") %>%
+    tidyr::separate(value, c("edicao", "date"), sep = "\\(") %>%
     dplyr::mutate(date = lubridate::dmy(date, locale = "pt_BR.UTF-8"),
                   date = as.Date(date))
 
@@ -87,11 +88,12 @@ tjba_link <- function(date) {
   stringr::str_c(u_dje, edicao)
 }
 
+# Get link for TJSC"s DJE
 tjsc_link <- function(date, booklet) {
 
-  date <- format(lubridate::as_date(date), '%d/%m/%Y')
+  date <- format(lubridate::as_date(date), "%d/%m/%Y")
 
-  u <- 'http://busca.tjsc.jus.br/dje-consulta/rest/diario/dia?dia='
+  u <- "http://busca.tjsc.jus.br/dje-consulta/rest/diario/dia?dia="
   edicao <- httr::GET(stringr::str_c(u, date)) %>%
     xml2::read_html() %>%
     stringr::str_extract("\"edicao\":[0-9]+") %>%
@@ -105,53 +107,54 @@ tjsc_link <- function(date, booklet) {
   )
 }
 
+# Get link for TJRN"s DJE
 tjrn_link <- function(date, booklet) {
   pega_jsf <- function(r) {
     r %>%
-      httr::content('text') %>%
+      httr::content("text") %>%
       xml2::read_html() %>%
-      rvest::html_nodes(xpath = '//input[contains(@id, "_64")]') %>%
-      rvest::html_attr('value')
+      rvest::html_nodes(xpath = "//input[contains(@id, "_64")]") %>%
+      rvest::html_attr("value")
   }
-  u_inicial <- 'http://www.diario.tjrn.jus.br/djonline/inicial.jsf'
+  u_inicial <- "http://www.diario.tjrn.jus.br/djonline/inicial.jsf"
   r_inicial <- httr::GET(u_inicial, httr::config(ssl_verifypeer = FALSE))
-  u <- 'https://www.diario.tjrn.jus.br/djonline/goto.jsf'
+  u <- "https://www.diario.tjrn.jus.br/djonline/goto.jsf"
   jsf <- r_inicial %>% pega_jsf()
   dados_pesq <- list(
-    'jsf_tree_64' = jsf[1],
-    'jsf_state_64' = jsf[2],
-    'jsf_viewid' = '/goto.jsp',
-    'menu:formMenu_SUBMIT' = '1',
-    'menu:formMenu:_link_hidden_' = 'menu:formMenu:_id16'
+    "jsf_tree_64" = jsf[1],
+    "jsf_state_64" = jsf[2],
+    "jsf_viewid" = "/goto.jsp",
+    "menu:formMenu_SUBMIT" = "1",
+    "menu:formMenu:_link_hidden_" = "menu:formMenu:_id16"
   )
   r_pesq <- httr::POST(u, body = dados_pesq,
                        httr::config(ssl_verifypeer = FALSE))
   jsf <- r_pesq %>% pega_jsf()
-  date_link0 <- format(as.Date(date), '%d/%m/%Y')
+  date_link0 <- format(as.Date(date), "%d/%m/%Y")
   dados0 <- list(
-    'jsf_tree_64' = jsf[1],
-    'jsf_state_64' = jsf[2],
-    'jsf_viewid' = '/goto.jsp',
-    'pesquisarEdicaoCompletaBean:pesquisa_:dataInicio' = date_link0,
-    'pesquisarEdicaoCompletaBean:pesquisa_:dataFim' = date_link0,
-    'pesquisarEdicaoCompletaBean:pesquisa_:cmdPesquisar' = 'Pesquisar',
-    'pesquisarEdicaoCompletaBean:pesquisa__SUBMIT' = '1',
-    'edicao' = '',
-    'pesquisarEdicaoCompletaBean:pesquisa_:_link_hidden_' = ''
+    "jsf_tree_64" = jsf[1],
+    "jsf_state_64" = jsf[2],
+    "jsf_viewid" = "/goto.jsp",
+    "pesquisarEdicaoCompletaBean:pesquisa_:dataInicio" = date_link0,
+    "pesquisarEdicaoCompletaBean:pesquisa_:dataFim" = date_link0,
+    "pesquisarEdicaoCompletaBean:pesquisa_:cmdPesquisar" = "Pesquisar",
+    "pesquisarEdicaoCompletaBean:pesquisa__SUBMIT" = "1",
+    "edicao" = "",
+    "pesquisarEdicaoCompletaBean:pesquisa_:_link_hidden_" = ""
   )
   r1 <- httr::POST(u, body = dados0,
                    httr::config(ssl_verifypeer = FALSE),
-                   encode = 'form')
+                   encode = "form")
   itens <- r1 %>%
-    httr::content('text') %>%
+    httr::content("text") %>%
     xml2::read_html() %>%
     rvest::html_nodes(
-      xpath = '//tbody[@id="pesquisarEdicaoCompletaBean:dados:tbody_element"]//a'
+      xpath = "//tbody[@id="pesquisarEdicaoCompletaBean:dados:tbody_element"]//a"
     ) %>% {
       if (length(.) == 2) {
-        id <- rvest::html_attr(., 'id')
-        nm <- rvest::html_attr(., 'onclick')
-        nm <- stringr::str_match(nm, "\\.value='([0-9]{8}_[A-Z]{3})'")[,2]
+        id <- rvest::html_attr(., "id")
+        nm <- rvest::html_attr(., "onclick")
+        nm <- stringr::str_match(nm, "\\.value="([0-9]{8}_[A-Z]{3})"")[,2]
         list(list(id[1], nm[1]), list(id[2], nm[2]))
       } else {
         FALSE
@@ -160,58 +163,60 @@ tjrn_link <- function(date, booklet) {
   if (is.logical(itens)) return(dplyr::data_frame(link = rep(u, 2), caderno = 1:2))
   jsf <- r1 %>% pega_jsf()
   dados <- lapply(itens, function(x) {
-    list('jsf_tree_64' = jsf[1],
-         'jsf_state_64' = jsf[2],
-         'jsf_viewid' = '/goto.jsp',
-         'linkDummyForm:_link_hidden_' = x[[1]],
-         'pesquisarEdicaoCompletaBean:scroll_1' = '',
-         'edicao' = x[[2]]
+    list("jsf_tree_64" = jsf[1],
+         "jsf_state_64" = jsf[2],
+         "jsf_viewid" = "/goto.jsp",
+         "linkDummyForm:_link_hidden_" = x[[1]],
+         "pesquisarEdicaoCompletaBean:scroll_1" = "",
+         "edicao" = x[[2]]
     )
   })
   links <- sapply(dados, function(x) {
     r <- httr::POST(u, body = x,
                     httr::config(ssl_verifypeer = FALSE),
-                    encode = 'form')
-    u_final <- 'https://www.diario.tjrn.jus.br/djonline/pages/edicao/edicaoVisualizadorTopo.jsf'
+                    encode = "form")
+    u_final <- "https://www.diario.tjrn.jus.br/djonline/pages/edicao/edicaoVisualizadorTopo.jsf"
     r_final <- httr::GET(u_final, httr::config(ssl_verifypeer = FALSE))
     r_final %>%
-      httr::content('text', encoding = 'ISO-8859-1') %>%
+      httr::content("text", encoding = "ISO-8859-1") %>%
       xml2::read_html() %>%
-      rvest::html_node('a') %>%
-      rvest::html_attr('href') %>%
-      {paste0('https://www.diario.tjrn.jus.br', gsub('\\', '/', ., fixed = T))}
+      rvest::html_node("a") %>%
+      rvest::html_attr("href") %>%
+      {paste0("https://www.diario.tjrn.jus.br", gsub("\\", "/", ., fixed = T))}
   })
   links <- dplyr::data_frame(link = links, caderno = 1:2)
   links$link[booklet]
 }
 
+# Get link for TJAC"s DJE
 tjac_link <- function(date) {
   date <- as.Date(date)
-  u <- sprintf('http://diario.tjac.jus.br/edicoes.php?Ano=%d&Mes=%d',
+  u <- sprintf("http://diario.tjac.jus.br/edicoes.php?Ano=%d&Mes=%d",
                lubridate::year(date), lubridate::month(date))
   r <- httr::GET(u)
   l <- r %>%
-    httr::content('text') %>%
+    httr::content("text") %>%
     xml2::read_html() %>%
-    rvest::html_nodes('table tr.texto_normal') %>% {
-      dates <- rvest::html_node(., 'a.texto_normal') %>%
+    rvest::html_nodes("table tr.texto_normal") %>% {
+      dates <- rvest::html_node(., "a.texto_normal") %>%
         rvest::html_text() %>%
         stringr::str_trim() %>%
-        purrr::map_chr(conv_months) %>%
+        purrr::map_chr(conv_month) %>%
         lubridate::dmy() %>%
         lubridate::as_date()
-      link <- rvest::html_node(., xpath = './/a[@title="Baixar"]') %>%
-        rvest::html_attr('href') %>%
-        {paste0('http://diario.tjac.jus.br', .)}
+      link <- rvest::html_node(., xpath = ".//a[@title="Baixar"]") %>%
+        rvest::html_attr("href") %>%
+        {paste0("http://diario.tjac.jus.br", .)}
       dplyr::data_frame(dates, link)
     } %>%
     dplyr::filter(dates %in% date) %>%
     with(link)
-  if (length(l) == 0) return('http://diario.tjac.jus.br/edicoes.php')
+  if (length(l) == 0) return("http://diario.tjac.jus.br/edicoes.php")
   l
 }
 
-conv_months <- function(str) {
+# Convert Portuguese months to number
+conv_month <- function(str) {
   str <- stringr::str_replace_all(str, " de ", "-")
   month <- stringr::str_extract(str, "[a-z]+")
 
@@ -228,35 +233,52 @@ conv_months <- function(str) {
     "outubro" = "10",
     "novembro" = "11",
     "dezembro" = "12")
-
   stringr::str_replace(str, "[a-z]+", month)
 }
 
 # Download DJE file
-download_pdf <- function(u, a, verbose = FALSE) {
-  if (file.exists(a)) {
-    if (verbose) cat('\narquivo ',  a, ' ja existe!\n')
-    return(dplyr::data_frame(result = 'exists'))
-  }
-  if (verbose) cat('\nbaixando ', a, '...', sep = '')
-  res <- tryCatch({
-    r <- suppressWarnings({
-      httr::GET(u, httr::write_disk(a, overwrite = TRUE),
-                httr::config(ssl_verifypeer = FALSE))
-    })
-    ct <- httr::headers(r)[['content-type']]
-    ct <- ifelse(is.null(ct), 'application', ct)
-    if (httr::status_code(r) == 200 && stringr::str_detect(ct, 'application')) {
-      if (verbose) cat('OK!\n')
-      return(dplyr::data_frame(result = 'ok'))
+download_pdf <- function(u_dje, file, verbose) {
+
+  # Don"t download if file exists
+  if (file.exists(file)) { return(dplyr::data_frame(result = "EXISTS")) }
+
+  # Set verbose variables
+  msg <- stringr::str_c("Downloading '", file, "'... ")
+  bkspc <- stringr::str_c(rep("\b", 51), collapse = "")
+
+  # Print download message
+  if (verbose) { message(msg) }
+
+  # Wrapper to download file
+  download <- purrr::possibly(function(u, f, verbose) {
+
+    # Download page
+    GET <- purrr::safely(httr::GET)
+    r <- GET(u, httr::write_disk(f), httr::config(ssl_verifypeer = FALSE))
+
+    # Return TIMEOUT
+    if(!is.null(r$error) && stringr::str_detect(r$error, "Timeout")) {
+      return(dplyr::data_frame(result = "TIMEOUT"))
     }
-  }, error = function(e) as.character(e))
-  if (stringr::str_detect(res, 'Timeout')) {
-    if (verbose) cat('ERRO!\n')
-    return(dplyr::data_frame(result = 'timeout'))
-  }
-  if (verbose) cat('ERRO!\n')
-  return(dplyr::data_frame(result = 'invalid dje'))
+
+    # Get content type
+    r <- r$result
+    ct <- httr::headers(r)[["content-type"]] %>%
+      ifelse(is.null(.), "application", .)
+
+    # Return OK
+    if (r$status_code == 200 && stringr::str_detect(ct, "application")) {
+      return(dplyr::data_frame(result = "OK"))
+    }
+  }, {
+
+    # Return ERROR
+    dplyr::data_frame(result = "ERROR")
+  })
+
+  # Collect result
+  result <- download(u, f, verbose)
+  if (verbose) { message(bkspc, msg, result[[1]], "!") }
+
+  return(result)
 }
-download_pdf <- purrr::possibly(
-  download_pdf, dplyr::data_frame(result = "error"))
