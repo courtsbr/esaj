a3305ac028189da0ea8b26a89d9809c86df13848
Caio Lente <ctlente@gmail.com>
2017-09-18 18:06:16 -0300

    Updated download_decision()

--- R/decision.R
+++ R/decision.R
@@ -1,10 +1,10 @@
 
 #' Download decision PDFs
-#' @param id A character vector with decision IDs
+#' @param decision A character vector with decision IDs
 #' @param path Path to directory where to save PDFs
 #' @param tj TJ to download decisions (only works with TJSP for now)
 #' @export
-download_decision <- function(id, path, tj = "tjsp") {
+download_decision <- function(decision, path, tj = "tjsp") {
 
   # Stop if TJ isn't TJSP
   stopifnot(tj == "tjsp")
@@ -14,20 +14,19 @@ download_decision <- function(id, path, tj = "tjsp") {
 
   # Download decisions
   dwld <- purrr::possibly(download_decision_, "")
-  p <- progress::progress_bar$new(total = length(id))
-  purrr::map_chr(id, ~{ dwld(.x, path); p$tick() })
+  purrr::map_chr(decision, dwld, path)
 }
 
-download_decision_ <- function(id, path, ntry = 10, verbose = FALSE) {
+download_decision_ <- function(decision, path, ntry = 10, verbose = FALSE) {
 
   # Download page with captcha
   captcha <- httr::GET(
     "https://esaj.tjsp.jus.br/cjsg/getArquivo.do",
-    query = list(cdAcordao = id, cdForo = 0),
+    query = list(cdAcordao = decision, cdForo = 0),
     httr::config(ssl_verifypeer = FALSE))
 
   # File where to save PDF
-  file <- stringr::str_c(normalizePath(path), "/", id, ".pdf")
+  file <- stringr::str_c(normalizePath(path), "/", decision, ".pdf")
 
   # Try to download PDF at most ntry times
   for (i in 1:ntry) {
@@ -43,7 +42,7 @@ download_decision_ <- function(id, path, ntry = 10, verbose = FALSE) {
     # Query for GET request
     query_get <- list(
       conversationId = "",
-      cdAcordao = id,
+      cdAcordao = decision,
       cdForo = 0,
       uuidCaptcha = captcha_uuid(f_captcha),
       vlCaptcha = break_rgb_captcha(f_captcha),
