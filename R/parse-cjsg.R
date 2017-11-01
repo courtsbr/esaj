08f7acc16e1579c28f06eec48204d54793e990f3
Julio Trecenti <julio.trecenti@gmail.com>
2017-11-01 18:59:58 -0200

    parallel parsing, progress bar and stop using dvec fun

--- R/parse-cjsg.R
+++ R/parse-cjsg.R
@@ -1,5 +1,4 @@
-parse_cjsg_um <- function(i, nodes) {
-  node <- nodes[[i]]
+parse_cjsg_one <- function(node) {
   trim <- stringr::str_trim
   id <- node %>%
     rvest::html_node('.ementaClass') %>%
@@ -8,8 +7,8 @@ parse_cjsg_um <- function(i, nodes) {
     stringr::str_replace_all('[^0-9]', '')
   infos <- node %>%
     rvest::html_node('.downloadEmenta') %>% {
-      tibble::tibble(n_processo = trim(rvest::html_text(.)),
-                     cd_acordao = rvest::html_attr(., 'cdacordao'))
+      tibble::tibble(id_lawsuit = trim(rvest::html_text(.)),
+                     id_decision = rvest::html_attr(., 'cdacordao'))
     }
   ca <- node %>%
     rvest::html_node('.assuntoClasse') %>%
@@ -31,42 +30,67 @@ parse_cjsg_um <- function(i, nodes) {
                   key = stringr::str_replace_all(key, '_d[eo]_', '_')) %>%
     tidyr::spread(key, val) %>%
     dplyr::bind_cols(infos) %>%
-    dplyr::mutate(id = id, classe_assunto = ca, txt_ementa = tsf) %>%
-    dplyr::select(id, cd_acordao, n_processo, dplyr::everything(), txt_ementa)
+    dplyr::mutate(id_page = id,
+                  class_subject = ca,
+                  txt_summary = tsf) %>%
+    dplyr::select(id_page,
+                  id_decision,
+                  id_lawsuit,
+                  class_subject,
+                  district = comarca,
+                  court = orgao_julgador,
+                  dt_decision = data_julgamento,
+                  dt_registration = data_registro,
+                  rapporteur = relatora,
+                  summary = ementa,
+                  txt_summary) %>%
+    tibble::as_tibble()
   tab_infos
 }
 
-parse_cjsg_arq <- function(arq) {
-  itens <- xml2::read_html(arq, encoding = 'UTF-8') %>%
+parse_cjsg_file <- function(file) {
+  safe_parse_cjsg_one <- purrr::possibly(parse_cjsg_one,
+                                         tibble::tibble(result = "error"))
+  items <- xml2::read_html(file, encoding = 'UTF-8') %>%
     rvest::html_nodes('.fundocinza1')
-  abjutils::dvec(parse_cjsg_um, 1:length(itens), nodes = itens, verbose = FALSE) %>%
-    dplyr::select(-item)
+  purrr::map_dfr(items, parse_cjsg_one)
 }
 
 #' @title CJSG parser
 #'
 #' @description Parser for files downloaded by [download_cjsg()].
 #'
-#' @param arqs Character vector with the paths of the files to be parsed
+#' @param files Character vector with the paths of the files to be parsed
+#' @param cores Number of cores to be used. Defaults to 1 to show progress bar.
 #'
 #' @return A tibble with the columns
 #' \itemize{
-#'   \item `arq` Name of the file
-#'   \item `id` ID found in the page read
-#'   \item `cd_acordao` Unique ID of the ruling
-#'   \item `n_processo` Number of the lawsuit (doesn't have to be unique)
-#'   \item `comarca` Name of the district
-#'   \item `data_julgamento` Date of the judgement (\%d/\%m/\%Y)
-#'   \item `data_registro` Date of registration in the system (\%d/\%m/\%Y)
-#'   \item `ementa` Summary of the ruling
-#'   \item `orgao_julgador` Body responsible for the appeal
-#'   \item `outros_numeros` Old/additional IDs
-#'   \item `relatora` Name of the rapporteur
-#'   \item `classe_assunto` Class/subject, separated by slashes
-#'   \item `txt_ementa` Text of the summary with no formatting
+#'   \item `file` Name of the file
+#'   \item `id_page` ID found in the page
+#'   \item `id_decision` Unique ID of the ruling
+#'   \item `id_lawsuit` Number of the lawsuit (doesn't have to be unique)
+#'   \item `district` Name of the district
+#'   \item `dt_decision` Date of the judgement (\%d/\%m/\%Y)
+#'   \item `dt_registration` Date of registration in the system (\%d/\%m/\%Y)
+#'   \item `summary` Summary of the ruling
+#'   \item `court` Body responsible for the appeal
+#'   \item `rapporteur` Name of the rapporteur
+#'   \item `class_subject` Class/subject, separated by slashes
+#'   \item `txt_summary` Text of the summary with no formatting
 #' }
 #' @export
-parse_cjsg <- function(arqs) {
-  abjutils::dvec(parse_cjsg_arq, arqs) %>%
-    dplyr::rename(arq = item)
+parse_cjsg <- function(files, cores = 1) {
+  names(files) <- files
+  safe_parse_cjsg_file <- purrr::possibly(parse_cjsg_file,
+                                          tibble::tibble(result = "error"))
+  if (cores == 1) {
+    p <- progress::progress_bar$new(total = length(files))
+    purrr::map_dfr(files, ~{
+      p$tick()
+      safe_parse_cjsg_file(.x)
+    }, .id = "file")
+  } else {
+    parallel::mclapply(files, safe_parse_cjsg_file, mc.cores = cores) %>%
+      dplyr::bind_rows(.id = "file")
+  }
 }
