3c5271ccba3e3d95d10ed5316f0f752a5679cb61
Caio Lente <ctlente@gmail.com>
2018-02-06 21:07:00 -0200

    Exported parsers

--- R/aux_parser.R
+++ R/aux_parser.R
@@ -1,28 +1,49 @@
 
+#' Parse parts
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_parts <- function(parser) {
   UseMethod("parse_parts", parser)
 }
 
+#' Parse data
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_data <- function(parser) {
   UseMethod("parse_data", parser)
 }
 
+#' Parse decisions
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_decisions <- function(parser) {
   UseMethod("parse_decisions", parser)
 }
 
+#' Parse movements
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_movs <- function(parser) {
   UseMethod("parse_movs", parser)
 }
 
+#' Parse history
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_hist <- function(parser) {
   UseMethod("parse_hist", parser)
 }
 
+#' Parse hearings
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_hearings <- function(parser) {
   UseMethod("parse_hearings", parser)
 }
 
+#' Parse police department
+#' @param parser A parser returned by [make_parser()]
+#' @export
 parse_pd <- function(parser) {
   UseMethod("parse_pd", parser)
 }
